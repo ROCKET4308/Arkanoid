89461abd1cedd445e1207fdc0b7cf36a07818eea
tanya598 <obrysnik.tanya@gmail.com>
2023-02-09 18:29:10 +0200

    Сторено функцію check_collision яка перевіряє чи мяч стикався з платформою

--- classes/Block.py
+++ classes/Block.py
@@ -7,3 +7,9 @@ class Block:
         self.height = height
         self.status = "intact"
 
+    def check_collision(self, ball):
+        if self.status == "intact":
+            if self.position[0] <= ball.position[0] <= self.position[0] + self.width:
+                if self.position[1] <= ball.position[1] <= self.position[1] + self.height:
+                    return True
+        return False
