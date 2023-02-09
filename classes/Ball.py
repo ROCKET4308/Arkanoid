c1f4ad429ef86dfaa1a7c31def722edaa7d0a17c
ROCKET228 <michkahap@gmail.com>
2023-02-09 18:34:50 +0200

    Add def move at class Ball

--- classes/Ball.py
+++ classes/Ball.py
@@ -1,4 +1,10 @@
 import pygame
 
+class Ball:
+    def __init__(self, position, speed):
+        self.position = position
+        self.speed = speed
 
-
+    def move(self):
+        self.position[0] += self.speed[0]
+        self.position[1] += self.speed[1]
