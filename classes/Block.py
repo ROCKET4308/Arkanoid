d68ee4aa2c80e033c60e6224d8483811be5c992b
tanya598 <obrysnik.tanya@gmail.com>
2023-02-09 18:25:29 +0200

    Сторено функцію ініціалізації класу "block"

--- classes/Block.py
+++ classes/Block.py
@@ -1,1 +1,9 @@
 import pygame
+
+class Block:
+    def __init__(self, position, width, height):
+        self.position = position
+        self.width = width
+        self.height = height
+        self.status = "intact"
+
