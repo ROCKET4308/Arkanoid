a8924182944649757910fe2dc8a50524ea7fc885
tanya598 <obrysnik.tanya@gmail.com>
2023-02-10 21:26:11 +0200

    Метод 'check_collision' перевіряє, чи не зіткнувся м'яч із платформою. Він повертає «Істина», якщо положення м’яча x і y знаходяться в межах платформи, і «Хибне» в іншому випадку.

--- classes/Paddle.py
+++ classes/Paddle.py
@@ -8,3 +8,9 @@ class Paddle:
 
     def move(self, x):
         self.position[0] += x
+
+    def check_collision(self, ball):
+        if self.position[0] <= ball.position[0] <= self.position[0] + self.width:
+            if self.position[1] <= ball.position[1] <= self.position[1] + self.height:
+                return True
+        return False
