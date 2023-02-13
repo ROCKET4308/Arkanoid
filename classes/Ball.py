00af59cd49cb386bb37de6882e8cdf115a2beb04
ROCKET228 <michkahap@gmail.com>
2023-02-13 21:52:07 +0200

    Make documentation for class Game and Ball ant their function

--- classes/Ball.py
+++ classes/Ball.py
@@ -1,10 +1,30 @@
 import pygame
 
 class Ball:
-    def __init__(self, position:List[int], speed:int):
+    """
+       Class representing a ball that can move.
+
+       Attributes:
+           position (list): A list of two integers representing the x and y coordinates of the ball's position.
+           speed (list): A list of two integers representing the x and y speed of the ball.
+
+       Methods:
+           move: Updates the position of the ball by adding the speed to the current position.
+    """
+    def __init__(self, position:List[int], speed:List[int]):
+        """
+           The constructor for the Ball class.
+
+           Parameters:
+                    position (list): A list of two integers representing the x and y coordinates of the ball's position.
+                    speed (list): A list of two integers representing the x and y speed of the ball.
+        """
         self.position = position
         self.speed = speed
 
     def move(self):
+        """
+            Updates the position of the ball by adding the speed to the current position.
+        """
         self.position[0] += self.speed[0]
         self.position[1] += self.speed[1]
--- classes/Game.py
+++ classes/Game.py
@@ -5,7 +5,29 @@ from classes.Block import Block
 from components.BlocksArray import blocks
 
 class Game:
+    """The Game class represents the Breakout game.
+
+        Attributes:
+            screen: The screen surface to display the game.
+            clock: The clock to control the game's framerate.
+            ball: The ball object in the game.
+            paddle: The paddle object in the game.
+            blocks: A list of block objects in the game.
+            score: The score object in the game.
+
+        Methods:
+            run: The main loop of the game. It updates and renders the game until the game is over.
+            update: Updates the state of the game objects.
+            render: Renders the game objects on the screen.
+            check_game_over: This method checks if the game is over by checking if the ball has fallen out of the screen or if all blocks have been destroyed.
+     """
+
     def __init__(self, screen_size):
+        """The constructor for the Game class.
+
+            Parameters:
+                screen_size: The size of the screen to display the game.
+        """
         pygame.init()
         self.screen = pygame.display.set_mode(screen_size)
         self.clock = pygame.time.Clock()
@@ -15,6 +37,7 @@ class Game:
 
 
     def run(self):
+        """The main loop of the game. It updates and renders the game until the game is over."""
         running = True
         while running:
             for event in pygame.event.get():
@@ -35,6 +58,7 @@ class Game:
         pygame.quit()
 
     def update(self):
+        """Updates the state of the game objects."""
         self.ball.move()
         if self.ball.position[0] <= 0 or self.ball.position[0] >= self.screen.get_width():
             self.ball.speed[0] = -self.ball.speed[0]
@@ -49,6 +73,7 @@ class Game:
 
 
     def render(self):
+        """Renders the game objects on the screen."""
         self.screen.fill((255, 255, 255))
         pygame.draw.circle(self.screen, (255, 0, 0), self.ball.position, 20)
         pygame.draw.rect(self.screen, (0, 0, 255), (self.paddle.position[0], self.paddle.position[1], self.paddle.width, self.paddle.height))
@@ -58,6 +83,12 @@ class Game:
         pygame.display.update()
 
     def check_game_over(self):
+        """
+            This method checks if the game is over by checking if the ball has fallen out of the screen or if all blocks have been destroyed.
+
+            Returns:
+                bool: True if the game is over, False otherwise.
+        """
         if self.ball.position[1] > self.screen.get_height():
             return True
         elif len([block for block in self.blocks if block.status == "intact"]) == 0:
