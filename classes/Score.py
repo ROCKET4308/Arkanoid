bb4b5c0fb04dea533459a226b06e71ed81338fa1
tanya598 <obrysnik.tanya@gmail.com>
2023-02-16 12:55:47 +0200

    Документація

--- classes/Score.py
+++ classes/Score.py
@@ -1,8 +1,23 @@
 import pygame
 
 class Score:
+    """
+    Class representing a score in the game.
+
+    Attributes:
+        score (int): The current score of the game.
+    """
     def __init__(self, score):
+        """
+        The constructor for the Score class.
+
+        Parameters:
+            score (int): The starting score for the game.
+        """
         self.score = score
 
     def increase(self):
+        """
+        Increases the score by 1.
+        """
         self.score += 1
